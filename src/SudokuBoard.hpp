2536cbfab3c6d4aa97221520e58022b565779a26
shrimpnoodles <hannahcierlak@gmail.com>
2021-05-27 20:35:48 -0700

    create game function

--- src/SudokuBoard.hpp
+++ src/SudokuBoard.hpp
@@ -14,10 +14,24 @@ public:
     void exitGame(){
 	//make entry would call exit game if 'entry' is ever q
 	//exit game then ends program
+		cout << "Goodbye!" << endl;
 		exit(0);
 	}
-    void createGame(); 
+    void createGame(){
+	string input;
+	cout << "Hello..." << endl;
+	cout << "Hard or easy game? (Please type ""hard"" or ""easy""" <<endl;
+	cin >> input;
+	
+	if(input == "hard"){
+		game = new EasyGame.generateGame();	
+	}
+	else if(input == "easy"){
+		game = new HardGame.generateGame();
+	}
+	
     void getSolution(){
+	solution = game.generateSolution();
 	for(int i=0; i < 9; ++i){
 		for(int j =0; j < 9; ++j){
 			cout << solution[i][j] << ' ';
