60f3095f04f2da7cb8abcc6572fd827a3101444e
vmeno0020 <vishalmenon20@gmail.com>
2021-05-27 20:42:20 -0700

    Implemented getScore, printBoard, makeEntry

--- src/SudokuBoard.hpp
+++ src/SudokuBoard.hpp
@@ -4,13 +4,33 @@
 #include "GameStrategy.hpp"
 #include "SudokuCell.hpp"
 
-
 class SudokuBoard {
 public:
     SudokuBoard();
-    int getScore();
-    void printBoard();
-    void makeEntry();
+    int getScore() {
+	return score;
+    }
+
+    void printBoard() {
+  	for(int i = 0; i < 9; i++) {
+    		for(int j = 0; j < 9; j++) {
+      			if(game == nullptr)
+				cout << ".";
+			else
+				cout << game[i][j];
+      				cout << "|";
+			}
+    				cout << endl;
+		}
+	}
+
+    void makeEntry() {
+	cout << "Make an entry." << endl;
+	cin >> userEntry;
+	if (isFilled(int row, int col) == true) {
+		cout << "No entry possible in this position.";
+		}
+	}
     void exitGame(); 
     void createGame(); 
     void getSolution(); 
@@ -19,8 +39,7 @@ protected:
     int[9][9] solution;
     SudokuCell*[9][9] game;
     int score;
-
-
-
+    bool keepPlaying;
+    int userEntry;		
 };
 #endif
