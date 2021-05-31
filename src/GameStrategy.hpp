e280370f2873fb62e4166ce3ce27b99fa9088475
vmeno0020 <vishalmenon20@gmail.com>
2021-05-30 23:00:18 -0700

    commit

--- src/GameStrategy.hpp
+++ src/GameStrategy.hpp
@@ -65,26 +65,19 @@ public:
 
 class EasyGame: public GameStrategy {
 public:
-    EasyGame() {}
-    virtual SudokuCell*[][] generateGame {
-        int x = 0;
-        int y = 0;
-        srand(0); //note: using same int for now for the sake of testing.
-        SudokuCell*[9][9] board; // note: might have to allocate memory for array first.
-        for (unsigned int i = 0; i < 9; i++) {
-            for (unsigned int j = 0; j < 9; j++) {
-                board[i][j] = new SudokuCell(grid[i][j], i, j);
-            }
-        }
-        for (unsigned i = 0; i < 29; i++) {
-            x = rand() % 9;
-            y = rand() % 9;
-            board[i][j]->SetCell(0);
-
-        }
-        return board;
-    }
-    virtual int[][]generateSolution { return grid; }
+    EasyGame(); 
+    virtual int[][] generateGame();
+    virtual int[][]generateSolution() { return grid; }
+protected:
+    int grid[9][9] = {{4,3,1,6,7,9,5,2,8},  
+                       {9,6,7,2,5,8,3,4,1},              
+		       {5,8,2,1,4,3,9,6,7},
+                       {6,5,9,8,1,7,2,3,4},
+                       {3,2,8,5,6,4,1,7,9},
+                       {7,1,4,9,3,2,8,5,6},
+                       {8,7,3,4,2,1,6,9,5},
+                       {1,4,5,3,9,6,7,8,2},
+                       {2,9,6,7,8,5,4,1,3}};
 
 };
 
--- src/SudokuBoard.hpp
+++ src/SudokuBoard.hpp
@@ -7,120 +7,26 @@
 using namespace std; 
 
 class SudokuBoard {
-public:
-    SudokuBoard() { }
-    int getScore() {
-	//idea : compare game and solution, if same score++
-	   
-	    //initialize solution
-	    //compare game and solution, if same score++
-	    for(int i=0; i < 9; ++i){
-                for(int j =0; j < 9; ++j){
-                      if(game[i][j] == solution[i][j]){
-			      	score++;
-		      }
-                }
-                	 if(game[i][j] == solution[i][j]){
-			      	score++;
-		      }
-        	}
-	}  
-	    return score;
-    }
+	public:
+   	 	 SudokuBoard();
 
-    void printBoard() {
-	if (game == nullptr) {return 0; }
-	cout << " |"; 
-	for (unsigned int k = 0; k < 9; k++) { 	
-	   cout << k++ << "|";  //prints row numbers 
-	}
-	cout << endl << "|"; 
-       for (unsigned int i = 0; i < 9; i++) {
-		cout << i++ << "|";
-	for (unsigned int j = 0; j < 9; j++) {
-		if (game[i][j]->getCell() != 0) {
-			cout << game[i][j]->getCell() << "|";
-		} else {
-			cout << " " << "|";
-		}
-	}
-	cout << endl;
-	if (i == 2 || i == 5 || i == 8) {
-		cout << "|=====|=====|=====|" << endl;
-	}
-	else{
-		cout << "|-----|-----|-----|" << endl;
-	}
-     }
-  		
-  }
-    void makeEntry() {
-	cout << "Make an entry." << endl;
-	cin >> userEntry;
-	if (isFilled(int row, int col) == true) {
-		cout << "No entry possible in this position.";
-  		 }
-	}
-    void exitGame(){
-	//make entry would call exit game if 'entry' is ever q
-	//exit game then ends program
-		cout << "Goodbye!" << endl;
-		exit(0);
-	}
-    void createGame(){
-	string input;
-	cout << "Hello..." << endl;
-	cout << "Hard or easy game? (Please type ""hard"" or ""easy"")" <<endl;
-	cin >> input;
+  	  	 int getScore()=0;
+ 		 void printBoard()=0;
 	
-	if(input != "hard" || input != "easy"){
-		while(input != "hard" || input != "easy"){
-			cout << "Invalid input. Please try again." << endl;
-			cin >> input;
-		}
-	}
+		 void getEntry(int row, int column, int input)=0;
+   		 void makeEntry()=0;
 
-	if(input == "hard"){
-		GameStrategy* hard = new HardGame(); 
-		game = hard->generateGame();
-		solution = hard->generateSolution();
-		//delete hard;  	
-	}
-	else if(input == "easy"){
-		GameStrategy* easy = new EasyGame(); 
-		game = easy->generateGame(); 
-		solution = easy->generateSolution(); 
-		//delete easy; 
-	}
-    }
-    void getSolution() {
-	int row = -1; 
-	int col = -1; 
-	cout << "Enter the digit of the row's solution you would like (1-9): " << endl;
-	while (row < 1 || row > 9 ) { 
-    		cin >> row; 
-		if (row < 1 || row > 9) { 
-			cout << "Invalid input! Please try again." << endl; 
-		}		
-	}
-	cout << "Enter the digit of the column's solution you would like (1-9): " << endl;
-        while (col < 1 || col > 9 ) {
-                cin >> col;
-                if (col < 1 || col > 9) {
-                        cout << "Invalid input! Please try again." << endl;
-                }
-        }
+		 void exitGame();
+   		 void createGame();
 
-	int soln = solution[row-1][col-1]; 
-	cout << "Solution (row " << row << ", column " << col << "): " << soln << endl; 
-	
-   }  	
-
-protected:
-    int[9][9] solution;
-    SudokuCell*[9][9] game;
-    int score;
-    bool keepPlaying;
-    int userEntry;		
+		 void getSolution()=0;
+	protected:
+		int gameBoard [9][9];
+		int userBoard[9][9];
+		int row;
+		int col;
+		int cellValue;
+		int score;
 };
 #endif
+
