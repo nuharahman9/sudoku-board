6dba0f871dfeadd9c3caa74d42cb28a28e1c9662
nuharahman9 <nrahm009@ucr.edu>
2021-05-30 20:53:19 -0700

    File organization

--- src/GameStrategy.hpp
+++ src/GameStrategy.hpp
@@ -37,30 +37,23 @@ protected:
                    };
 
 };
-
 class HardGame: public GameStrategy {
 public:
-    HardGame() {}
-    virtual SudokuCell*[][] generateGame {
-        int x = 0;
-        int y = 0;
-        srand(0); //note: using same int for now for the sake of testing.
-        SudokuCell*[9][9] board; // note: might have to allocate memory for array first.
-        for (unsigned int i = 0; i < 9; i++) {
-            for (unsigned int j = 0; j < 9; j++) {
-                board[i][j] = new SudokuCell(grid[i][j], i, j);
-            }
-        }
-        for (unsigned i = 0; i < 57; i++) {
-            x = rand() % 9;
-            y = rand() % 9;
-            board[i][j]->SetCell(0);
+    HardGame();
+    virtual int[][] generateGame();
+    virtual int[][] generateSolution() {return grid;}
 
-        }
-        return board;
-    }
-    virtual int[][]generateSolution { return grid; }
-
+protected:
+     int grid[9][9] = {{5, 3, 4, 6, 7, 8, 9, 1, 2},
+                       {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                       {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                       {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                       {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                       {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                       {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                       {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                       {3, 4, 5, 2, 8, 6, 1, 7, 9}
+                       };
 };
 
 class EasyGame: public GameStrategy {
