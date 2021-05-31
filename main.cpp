302d1d5c0d3cea0f44b5df7d61eb346b475e4cc7
vmeno0020 <vishalmenon20@gmail.com>
2021-05-31 15:51:09 -0700

    merge error

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "src/SudokuBoard.hpp"
 
 int main() {
-	SudokuBoard *test = test->getInstance();
-	
-}
+        SudokuBoard *test = test->getInstance();
+        test->createGame();
+        test->printBoard();
+        test->makeEntry();
+        test->printBoard();
+        test->getSolution();
+}	
