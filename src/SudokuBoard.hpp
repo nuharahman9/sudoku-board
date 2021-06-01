64ce2d8a63e2d5e6682ef58f372748f273c5b902
nuharahman9 <nrahm009@ucr.edu>
2021-06-01 03:09:09 -0700

    Fixed memory leaks

--- src/SudokuBoard.hpp
+++ src/SudokuBoard.hpp
@@ -2,14 +2,14 @@
 #define __SUDOKUBOARD_HPP__
 #include <iostream>
 //#include "GameStrategy.hpp"
-#include "EasyHard.hpp"
+#include "EasyHard.hpp" 
 #include <string>
 
 using namespace std; 
 
 class SudokuBoard {
 	public:
-		 static SudokuBoard* getInstance();
+		 static SudokuBoard*  getInstance();
 		 
 		 ~SudokuBoard();
   	  	 int getScore();
@@ -17,29 +17,23 @@ class SudokuBoard {
 	
 		 void getEntry(int row, int column, int input);
    		 void makeEntry();
-
 		 void exitGame();
    		 void createGame();
-
+		 static void StaticDestructor(); 
 		 void getSolution();
 		
-	//functions for testing
-		 void setDifficulty(string testString);
-		 bool isEasy();
-		 bool isHard();
-	protected:
+	protected: 
 		int gameBoard [9][9];
 		int userBoard [9][9];
 		int row = -1;
-		int col = -1;
+		int col = -1; 
 		int emptySlots = 0; 
-		int cellValue;
-		static SudokuBoard* instance;
-		string difficulty;
-		bool easy;
-		bool hard;
-	private:
+		int cellValue = 0;
+		static SudokuBoard* instance; 
+		string difficulty = ""; 
+	private: 
 		SudokuBoard(); /* private constructor to prevent instancing */
 };
+
 #endif
 
